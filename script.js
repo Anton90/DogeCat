4c6d0bd561bdb6b15201987e035e188fb43879d0
Anton Everaert <antoneveraert@gmail.com>
2019-03-22 15:03:45 +0100

    added hover image

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 window.onload = function doggo() 
-	{document.getElementById('doge').innerHTML = 'Woof!';}
+{
+	document.getElementById('doge').innerHTML = 'Woof!';
+}
+
+document.getElementById("doge").onmouseover = function() {over()};
+document.getElementById("doge").onmouseout = function() {out()};
+
+
+function over() 
+{
+    document.getElementById('doge').innerHTML = '<img src="dogicon.png" />';
+}
+
+function out()
+{
+	document.getElementById('doge').innerHTML = 'Woof!';
+}
+
